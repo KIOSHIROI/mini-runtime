2ec314a46439e5a90797b64ad918f10a04769853
kioshiro <2764999599@qq.com>
2026-05-31 17:52:38 +0800

    add Metrics

--- main.py
+++ main.py
@@ -2,26 +2,49 @@ import asyncio
 from asyncio import Queue
 from dataclasses import dataclass 
 
-
 @dataclass
 class Request:
     request_id: int
     prompt: str
     submit_time: float
+    
+    start_time: float | None = None
+    finish_time: float | None = None
 
 async def producer(queue: Queue):
     for i in range(10):
         request = Request(request_id=i, prompt=f"request-{i}", submit_time=asyncio.get_event_loop().time())
         await queue.put(request)
-        print(f"submit {request}")
+        print(f"submit {request.request_id}")
         await asyncio.sleep(1)
     
 async def worker(worker_id: int, queue: Queue):
+    loop = asyncio.get_running_loop()
     while True:
         request = await queue.get()
-        print(f"Worker-{worker_id} processing {request}")
+        request.start_time = loop.time()
+        print(f"Worker-{worker_id} processing {request.request_id}")
         await asyncio.sleep(3)
-        print(f"Worker-{worker_id} finish {request}")
+        request.finish_time = loop.time()
+        # print(f"Worker-{worker_id} finish {request}")
+        queue_wait = (
+            request.start_time
+            - request.submit_time
+        )
+        service_time = (
+            request.finish_time
+            - request.start_time
+        )
+        total_latency = (
+            request.finish_time
+            - request.submit_time
+        )
+        print(
+            f"[{request.request_id}] "
+            f"wait={queue_wait:.2f}s "
+            f"service={service_time:.2f}s "
+            f"total={total_latency:.2f}s"
+        )
         queue.task_done()
 
 async def main():
